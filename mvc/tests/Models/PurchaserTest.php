fe62d703137c8daab6843b845e876dd092c12416
Sveta Nazarova <s.nazarova@godeltech.com>
2019-05-28 17:51:38 +0300

    Add booksProvider into the test

--- mvc/tests/Models/PurchaserTest.php
+++ mvc/tests/Models/PurchaserTest.php
@@ -7,24 +7,64 @@ use PHPUnit\Framework\TestCase;
 
 class PurchaserTest extends TestCase
 {
-    public function testGetBooks()
+    /**
+     * @dataProvider booksProvider
+     */
+    public function testGetBooks($data)
     {
         $bookStore = new BookStore();
         $purchaser = new Purchaser($bookStore);
-        $price = $purchaser->getBooks([
+        $price = $purchaser->getBooks($data['array']);
+        $this->assertEquals($data['result'], $price);
+    }
+
+    /**
+     * @return array
+     */
+    public function booksProvider()
+    {
+        return [
             [
-                'id' => 1,
-                'copies' => 3
+                [
+                    'array' => [
+                        [
+                            'id' => 1,
+                            'copies' => 1
+                        ],
+                    ],
+                    'result' => 8,
+                ],
+                [
+                    'array' => [
+                        [
+                            'id' => 1,
+                            'copies' => 3
+                        ],
+                        [
+                            'id' => 2,
+                            'copies' => 3
+                        ],
+                    ],
+                    'result' => 8*2*0.95+(2+2)*8,
+                ],
+                [
+                    'array' => [
+                        [
+                            'id' => 1,
+                            'copies' => 3
+                        ],
+                        [
+                            'id' => 2,
+                            'copies' => 10
+                        ],
+                        [
+                            'id' => 3,
+                            'copies' => 1
+                        ],
+                    ],
+                    'result' => 8*3*0.9+(2+9)*8,
+                ],
             ],
-            [
-                'id' => 2,
-                'copies' => 2
-            ],
-            [
-                'id' => 3,
-                'copies' => 5
-            ],
-        ]);
-        $this->assertEquals(8*3*0.9+(2+1+4)*8, $price);
+        ];
     }
 }
