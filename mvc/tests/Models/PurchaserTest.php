1e0b9a261179f0d1e56884056e2e8028bbd02fc8
Sveta Nazarova <s.nazarova@godeltech.com>
2019-05-28 18:14:43 +0300

    Refactor dataProvider

--- mvc/tests/Models/PurchaserTest.php
+++ mvc/tests/Models/PurchaserTest.php
@@ -10,12 +10,12 @@ class PurchaserTest extends TestCase
     /**
      * @dataProvider booksProvider
      */
-    public function testGetBooks($data)
+    public function testGetBooks($books, $result)
     {
         $bookStore = new BookStore();
         $purchaser = new Purchaser($bookStore);
-        $price = $purchaser->getBooks($data['array']);
-        $this->assertEquals($data['result'], $price);
+        $price = $purchaser->getBooks($books);
+        $this->assertEquals($result, $price);
     }
 
     /**
@@ -24,46 +24,53 @@ class PurchaserTest extends TestCase
     public function booksProvider()
     {
         return [
-            [
-                [
-                    'array' => [
-                        [
-                            'id' => 1,
-                            'copies' => 1
-                        ],
+            'oneBookOneCopy' => [
+                'books' => [
+                    [
+                        'id' => 1,
+                        'copies' => 1
                     ],
-                    'result' => 8,
                 ],
-                [
-                    'array' => [
-                        [
-                            'id' => 1,
-                            'copies' => 3
-                        ],
-                        [
-                            'id' => 2,
-                            'copies' => 3
-                        ],
+                'result' => 8,
+            ],
+            'oneBookSeveralCopies' => [
+                'books' => [
+                    [
+                        'id' => 1,
+                        'copies' => 3
                     ],
-                    'result' => 8*2*0.95+(2+2)*8,
                 ],
-                [
-                    'array' => [
-                        [
-                            'id' => 1,
-                            'copies' => 3
-                        ],
-                        [
-                            'id' => 2,
-                            'copies' => 10
-                        ],
-                        [
-                            'id' => 3,
-                            'copies' => 1
-                        ],
+                'result' => 8*3,
+            ],
+            'severalBooksOneCopy' => [
+                'books' => [
+                    [
+                        'id' => 1,
+                        'copies' => 1
                     ],
-                    'result' => 8*3*0.9+(2+9)*8,
+                    [
+                        'id' => 2,
+                        'copies' => 1
+                    ],
                 ],
+                'result' => 8*2*0.95,
+            ],
+            'severalBooksSeveralCopies' => [
+                'books' => [
+                    [
+                        'id' => 1,
+                        'copies' => 3
+                    ],
+                    [
+                        'id' => 2,
+                        'copies' => 10
+                    ],
+                    [
+                        'id' => 3,
+                        'copies' => 1
+                    ],
+                ],
+                'result' => 8*3*0.9+(2+9)*8,
             ],
         ];
     }
